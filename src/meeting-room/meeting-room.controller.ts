177dac3af309988d45a010fc8a46dcd7e75bcca7
Sunj2000 <3014137637@qq.com>
2026-01-06 11:13:35 +0800

    s

--- src/meeting-room/meeting-room.controller.ts
+++ src/meeting-room/meeting-room.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { MeetingRoomService } from './meeting-room.service';
 
 @Controller('meeting-room')
 export class MeetingRoomController {
   constructor(private readonly meetingRoomService: MeetingRoomService) {}
+
+  @Post('init-data')
+  async initData() {
+    return this.meetingRoomService.initData();
+  }
 }
